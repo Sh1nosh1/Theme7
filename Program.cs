75e1b9c0ced8b8a477cd677096dde8c95d8f0853
Sh1nosh1 <shindanotamashi@gmail.com>
2023-01-29 19:38:06 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Task 1
+
+                /*for (int i = 1; i <= 18; i++)
+                {
+                    Console.Write('#');
+                }
+                Console.ReadKey();*/
+
+                /*int i = 0;
+                while (i < 18)
+                {
+                    Console.Write('#');
+                    i++;
+                }
+                Console.ReadKey();*/
+
+                /*int i = 0;
+                do
+                {
+                    Console.Write('#');
+                    i++;
+                }
+                while (i < 18);
+                Console.ReadKey();*/
+
+                //Task 2
+
+                /*for (int i = 1; i <= 25; i++)
+                {
+                    Console.WriteLine('#');
+                }
+                Console.ReadKey();*/
+
+                /*int i = 0;
+                while (i < 24)
+                {
+                    Console.WriteLine('#');
+                    i++;
+                }
+                Console.ReadKey();*/
+
+                /*int i = 0;
+                do
+                {
+                    Console.WriteLine('#');
+                    i++;
+                }
+                while (i < 24);
+                Console.ReadKey();*/
+
+                //Task 3
+
+                /*for(int i=0;i<4;i++)
+                {
+                    Console.WriteLine(' ');
+                }
+                for (int i = 1; i < 20; i++) 
+                {
+                    Console.Write('@');
+                }
+                Console.ReadKey();*/
+
+                /*int i = 0;
+                do
+                {
+                    Console.WriteLine(' ');
+                    i++;
+                }
+                while (i < 4);
+                int j = 0;
+                do
+                {
+                    Console.Write('#');
+                    j++;
+                }
+                while (j < 20);
+                Console.ReadKey();*/
+
+                /*int i = 0;
+                while (i < 4)
+                {
+                    Console.WriteLine(' ');
+                    i++;
+                }
+                int j = 0;
+                while (j < 20)
+                {
+                    Console.Write('#');
+                    j++;
+                }
+                Console.ReadKey();*/
+
+                //Task 4
+
+                /*for(int i=0;i<4;i++)
+                {
+                    Console.Write(' ');
+                }
+                for (int i = 0; i < 20; i++) 
+                {
+                    Console.Write('@');
+                }
+                Console.ReadKey();*/
+
+                /*int i = 0;
+                do
+                {
+                    Console.Write(' ');
+                    i++;
+                }
+                while (i < 4);
+                int j = 0;
+                do
+                {
+                    Console.Write('@');
+                    j++;
+                }
+                while (j < 20);
+                Console.ReadKey();*/
+
+                /*int i = 0;
+                while (i < 4)
+                {
+                    Console.Write(' ');
+                    i++;
+                }
+                int j = 0;
+                while (j < 20)
+                {
+                    Console.Write('@');
+                    j++;
+                }
+                Console.ReadKey();*/
+
+                //Task 5
+
+                /*for(int i=0;i<5;i++)
+                {
+                    for(int j = 0; j < 15; j++)
+                    {
+                        Console.Write('!');
+                    }
+                    Console.WriteLine();
+                }
+                Console.ReadKey();*/
+
+                /*int a = 0;
+                while(a<5)
+                {
+                    int i = 0;
+                    while(i < 15)
+                    {
+                        Console.Write('!');
+                        i++;
+                    }
+                    Console.WriteLine();
+                    a++;
+                }
+                Console.ReadKey();*/
+
+                /*int a = 0;
+                do
+                {
+                    int i = 0;
+                    while (i < 15)
+                    {
+                        Console.Write('!');
+                        i++;
+                    }
+                    Console.WriteLine();
+                    a++;
+                }
+                while (a < 5);
+                Console.ReadKey();*/
+
+                //Task 6
+
+                /*for(int i=0;i < 4;i++)
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        Console.Write('&');
+                    }
+                    Console.WriteLine("\n");
+                }
+                Console.ReadKey();*/
+
+                /*int a = 0;
+                while (a < 4)
+                {
+                    int i=0;
+                    while(i < 7)
+                    {
+                        Console.Write('&');
+                        i++;
+                    }
+                    Console.WriteLine("\n");
+                    a++;
+                }
+                Console.ReadKey(); */
+
+                /*int a = 0;
+                do
+                {
+                    int i = 0;
+                    while (i < 7)
+                    {
+                        Console.Write('&');
+                        i++;
+                    }
+                    Console.WriteLine("\n");
+                    a++;
+                }
+                while (a < 4);
+                Console.ReadKey(); */
+            }
+            catch
+            {
+                Console.WriteLine("Вы где-то ошиблись...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
